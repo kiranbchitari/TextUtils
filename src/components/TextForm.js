bb7c584ac1dfd85fd085c26946b2094abb9b83ae
kiranbchitari <kiranbchitari@gmail.com>
2021-08-30 01:25:18 +0530

    Minor changes to support github pages

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -57,8 +57,8 @@ export default function TextForm(props) {
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008 *  text.split(" ").length} Minutes read</p>
+            <p>{text===""?0:text.split(" ").length} words and {text.length} characters</p>
+            <p>{text===""?0:0.008 *  text.split(" ").length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
         </div>
